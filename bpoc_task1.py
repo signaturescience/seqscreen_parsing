9a0180d9f7d05e5761c81ae57a4089cd4c82393b
winniieeli <37006584+winniieeli@users.noreply.github.com>
2020-06-22 15:03:43 -0500

    task 1 and 2

--- bpoc_task1.py
+++ bpoc_task1.py
@@ -1,52 +1,54 @@
-from collections import defaultdict
-import csv
-import pandas as pd
-import argparse
+"""
+REU_2020 microbes task 1
+https://trello.com/b/tDbNjSfc/reu2020
+
+@author: wwl3
+"""
+import os
 import pathlib
+import argparse
+import pandas as pd
 
 # parse the inputted .tsv file (needs to be full path)
 parser = argparse.ArgumentParser()
 parser.add_argument("input_file", type=str, help="input a .tsv file")
 args = parser.parse_args()
-filename = pathlib.Path(args.input_file)
-
+filename = pathlib.PurePath(args.input_file).stem 
+output_dir = "outputs/"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+f_out = open(f"{output_dir}{filename}_summary.txt", "w") # summary file
 
 # remove rows where there are no bpocs, create a revised file
-df = pd.read_csv(filename, sep='\t', dtype=str)
+df = pd.read_csv(filename + ".tsv", sep='\t', dtype=str)
 
 # filename_short = str(filename).split('/')[-1].split('.')[-2]
 
 bpocs = ["adhesion", "secretion", "host_cell_death",
-			"antibiotic", "invasion", "evasion",
-			"cytotoxicity", "degrade_ecm", "disable_organ"]
-elems = ["taxid","organism","gene_name","uniprot", "uniprot evalue"]
+         "antibiotic", "invasion", "evasion",
+         "cytotoxicity", "degrade_ecm", "disable_organ"]
 
-df_bpocs = df[df[bpocs].replace('-',0).astype(int).sum(1)>0]
-df_bpocs.to_csv(f"{filename}_revised.tsv", sep='\t', index = False)
+elems = ["taxid", "organism", "gene_name", "uniprot", "uniprot evalue"]
 
-# What taxid, organism, gene_name, uniprot, and uniprot evalues 
+df_bpocs = df[df[bpocs].replace('-', 0).astype(int).sum(1) > 0]
+df_bpocs.to_csv(f"{filename}_revised.tsv", sep='\t', index=False)
+
+# What taxid, organism, gene_name, uniprot, and uniprot evalues
 # were assigned to the BPoCs within the sample?
-f_out = open(f"{filename}_summary.txt", "w")
 bpoc_counts = df_bpocs[bpocs].astype(int).sum(0)
 f_out.write(bpoc_counts.to_string())
 f_out.write("\n")
 f_out.write(f"Percentage of bpoc in sample:{len(df_bpocs.index)/len(df.index)}")
 f_out.write("\n\n")
 
-# TODO: process multi taxids
-# What taxid, organism, gene_name, uniprot, and uniprot evalues were assigned to the BPoCs within the sample?
+# What taxid, organism, gene_name, uniprot, and uniprot
+# evalues were assigned to the BPoCs within the sample?
 f_out.write("Taxid, organism, gene_name, uniprot, and uniprot evalues per BPoC:")
 for bpoc in bpocs:
-    if(bpoc_counts[bpoc]>0):
-        bpoc_elems = df_bpocs[df_bpocs[bpoc].astype(int)>0][elems]
+    if bpoc_counts[bpoc] > 0:
+        bpoc_elems = df_bpocs[df_bpocs[bpoc].astype(int) > 0][elems]
         f_out.write(f"\n{bpoc} \n")
         elems_in_bpoc = pd.Series(bpoc_elems.transpose().to_numpy().tolist(),
                                   index=bpoc_elems.columns).apply(lambda x: ', '.join(set(x)))
         f_out.write(f"{elems_in_bpoc.to_string()} \n")
 f_out.close()
-
-
-
-
-
-
--- go_term_query.py
+++ go_term_query.py
@@ -1,24 +1,20 @@
-# End users interested in specific GO terms would like to retrieve 
-#a list of all the rows that contain a specific GO term ID, or a 
-#list of GO term IDs of interest to them. It would be useful to 
-#summarize the proteins and organisms associated with each of the 
-#GO terms of interest in a SeqScreen output file.
+"""
+REU_2020 microbes task 2
+https://trello.com/b/tDbNjSfc/reu2020
 
-# How could a capability be setup for users to query specific 
-# GO terms within a sample, and return all of the information associated 
-#with each of those GO terms (e.g., organism, gene_name, uniprot)?
+@author: wwl3
+"""
 
 # What visualizations could be created?
-from collections import defaultdict
-import csv
-import pandas as pd
-import argparse
 import pathlib
+import argparse
+import pandas as pd
 
 # parse the inputted .tsv file (needs to be full path)
 parser = argparse.ArgumentParser()
 parser.add_argument("input_file", type=str, help="input a .tsv file")
-parser.add_argument("go_numbers", type=str, help="input go numbers separated by semicolons, with no spaces")
+parser.add_argument(
+    "go_numbers", type=str, help="input go numbers separated by semicolons, with no spaces")
 args = parser.parse_args()
 filename = pathlib.Path(args.input_file)
 go_nums = args.go_numbers.split(';')
@@ -28,22 +24,40 @@ go_nums = args.go_numbers.split(';')
 df = pd.read_csv(filename, sep='\t', dtype=str)
 
 # filename_short = str(filename).split('/')[-1].split('.')[-2]
-columns = ["taxid", "organism", "gene_name"]
+columns = ["taxid", "organism", "gene_name", "uniprot"]
 
 for go_num in go_nums:
-	# create a new .tsv file with just rows with that go number
-	df_go = df[df["go"].apply(lambda x: go_num in x)]
-	df_go.to_csv(f"{filename}_go_num_{go_num}_revised.tsv", sep='\t', index = False)
+    # create a new .tsv file with just rows with that go number
+    df_go = df[df["go"].apply(lambda x: go_num in x)]
+    df_go.to_csv(f"{filename}_go_num_{go_num}_revised.tsv", sep='\t', index=False)
 
-	# create a text file  of summary statistics for that go number
-	f_out = open(f"{filename}_go_num_{go_num}_summary.txt", "w")
-	f_out.write(f"Information associated with go term {go_num} for sample: {filename}")
-	f_out.write("\n\n")
-	f_out.write(f"Percentage of rows with go term {go_num} in sample:{len(df_go.index)/len(df.index)}")
-	f_out.write("\n\n")
-	# summarize the proteins and organisms
-	for col in columns:
-		df_go_counts = df_go.groupby(col).count().loc[:, "query"].sort_values(ascending=False)
-		f_out.write(f"{df_go_counts.to_string()} \n\n")
-	f_out.close()
+    # create a text file  of summary statistics for that go number
+    f_out = open(f"{filename}_go_num_{go_num}_summary.txt", "w")
+    f_out.write(f"Information associated with go term {go_num} for sample: {filename}")
+    f_out.write("\n\n")
+    f_out.write(
+        f"Percentage of rows with go term {go_num} in sample:{len(df_go.index)/len(df.index)}")
+    f_out.write("\n\n")
 
+    # summarize the proteins and organisms
+    for col in columns:
+        # if col == "taxid":
+        #   df_go_counts = df_go.groupby("multi_taxids_confidence").
+        #   .apply({
+        #       lambda x:
+  #               taxids = []
+        #       data = x.split(",")
+        #       maximum = 0
+        #       for dat in data:
+        #           taxid_conf = dat.split(":")
+        #           if taxid_conf[1] > maximum:
+        #               maximum = taxid_conf[1]
+        #       for dat in data:
+        #           taxid_conf = dat.split(":")
+        #           if taxid_conf[1] >= maximum:
+        #               taxixds.append(taxid_conf[0]))
+        #   .count().loc[:, "query"].sort_values(ascending=False)
+            # count().loc[:, "query"].sort_values(ascending=False)
+        df_go_counts = df_go.groupby(col).count().loc[:, "query"].sort_values(ascending=False)
+        f_out.write(f"{df_go_counts.to_string()} \n\n")
+    f_out.close()
