b9b64e132214fbef7570b1ee01851932bfaa4da2
winniieeli <37006584+winniieeli@users.noreply.github.com>
2020-06-23 12:34:52 -0500

    put task 1 into a file

--- bpoc_task1.py
+++ bpoc_task1.py
@@ -9,46 +9,49 @@ import pathlib
 import argparse
 import pandas as pd
 
-# parse the inputted .tsv file (needs to be full path)
-parser = argparse.ArgumentParser()
-parser.add_argument("input_file", type=str, help="input a .tsv file")
-args = parser.parse_args()
-filename = pathlib.PurePath(args.input_file).stem 
-output_dir = "outputs/"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-f_out = open(f"{output_dir}{filename}_summary.txt", "w") # summary file
+def bpoc_parse(df, filename, output_dir, f_out):
+    bpocs = ["adhesion", "secretion", "host_cell_death",
+             "antibiotic", "invasion", "evasion",
+             "cytotoxicity", "degrade_ecm", "disable_organ"]
 
-# remove rows where there are no bpocs, create a revised file
-df = pd.read_csv(filename + ".tsv", sep='\t', dtype=str)
+    elems = ["taxid", "organism", "gene_name", "uniprot", "uniprot evalue"]
 
-# filename_short = str(filename).split('/')[-1].split('.')[-2]
+    df_bpocs = df[df[bpocs].replace('-', 0).astype(int).sum(1) > 0]
+    df_bpocs.to_csv(f"{filename}_revised.tsv", sep='\t', index=False)
 
-bpocs = ["adhesion", "secretion", "host_cell_death",
-         "antibiotic", "invasion", "evasion",
-         "cytotoxicity", "degrade_ecm", "disable_organ"]
+    # What taxid, organism, gene_name, uniprot, and uniprot evalues
+    # were assigned to the BPoCs within the sample?
+    bpoc_counts = df_bpocs[bpocs].astype(int).sum(0)
+    f_out.write(bpoc_counts.to_string())
+    f_out.write("\n")
+    f_out.write(f"Percentage of bpoc in sample:{len(df_bpocs.index)/len(df.index)}")
+    f_out.write("\n\n")
 
-elems = ["taxid", "organism", "gene_name", "uniprot", "uniprot evalue"]
+    # What taxid, organism, gene_name, uniprot, and uniprot
+    # evalues were assigned to the BPoCs within the sample?
+    f_out.write("Taxid, organism, gene_name, uniprot, and uniprot evalues per BPoC:")
+    for bpoc in bpocs:
+        if bpoc_counts[bpoc] > 0:
+            bpoc_elems = df_bpocs[df_bpocs[bpoc].astype(int) > 0][elems]
+            f_out.write(f"\n{bpoc} \n")
+            elems_in_bpoc = pd.Series(bpoc_elems.transpose().to_numpy().tolist(),
+                                      index=bpoc_elems.columns).apply(lambda x: ', '.join(set(x)))
+            f_out.write(f"{elems_in_bpoc.to_string()} \n")
+    f_out.close()
 
-df_bpocs = df[df[bpocs].replace('-', 0).astype(int).sum(1) > 0]
-df_bpocs.to_csv(f"{filename}_revised.tsv", sep='\t', index=False)
+def main():
+    # parse the inputted .tsv file (needs to be full path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", type=str, help="input a .tsv file")
+    args = parser.parse_args()
+    filename = pathlib.PurePath(args.input_file).stem
+    output_dir = "outputs/"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    f_out = open(f"{output_dir}{filename}_summary.txt", "w") # summary file
 
-# What taxid, organism, gene_name, uniprot, and uniprot evalues
-# were assigned to the BPoCs within the sample?
-bpoc_counts = df_bpocs[bpocs].astype(int).sum(0)
-f_out.write(bpoc_counts.to_string())
-f_out.write("\n")
-f_out.write(f"Percentage of bpoc in sample:{len(df_bpocs.index)/len(df.index)}")
-f_out.write("\n\n")
+    # remove rows where there are no bpocs, create a revised file
+    df = pd.read_csv(pathlib.Path(args.input_file), sep='\t', dtype=str)
+    bpoc_parse(df, filename, output_dir, f_out)
 
-# What taxid, organism, gene_name, uniprot, and uniprot
-# evalues were assigned to the BPoCs within the sample?
-f_out.write("Taxid, organism, gene_name, uniprot, and uniprot evalues per BPoC:")
-for bpoc in bpocs:
-    if bpoc_counts[bpoc] > 0:
-        bpoc_elems = df_bpocs[df_bpocs[bpoc].astype(int) > 0][elems]
-        f_out.write(f"\n{bpoc} \n")
-        elems_in_bpoc = pd.Series(bpoc_elems.transpose().to_numpy().tolist(),
-                                  index=bpoc_elems.columns).apply(lambda x: ', '.join(set(x)))
-        f_out.write(f"{elems_in_bpoc.to_string()} \n")
-f_out.close()
+main()
