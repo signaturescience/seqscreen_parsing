a0795313ae903a26a0d69ba1434eb3ae37bd7ddb
Kristen Curry <kdc10@rice.edu>
2020-06-26 14:31:43 -0500

    correct new line pylint error

--- seqscreen_parse_utils.py
+++ seqscreen_parse_utils.py
@@ -1,1 +1,6 @@
-# -*- coding: utf-8 -*-"""seqscreen_parse_utils.pyAll functions used for parsing scripts in this repo."""
+# -*- coding: utf-8 -*-
+"""
+seqscreen_parse_utils.py
+
+All functions used for parsing scripts in this repo.
+"""
--- taxid_parse.py
+++ taxid_parse.py
@@ -1,1 +1,7 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""taxid_parse.pyScript to extract information regarding taxon abundances in sequences."""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+taxid_parse.py
+
+Script to extract information regarding taxon abundances in sequences.
+"""
