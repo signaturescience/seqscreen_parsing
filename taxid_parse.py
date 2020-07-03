b9609ff3b38d9a0fffbf20f3c271384034a9dd9f
carlaaysem <aysemsipahioglu@gmail.com>
2020-07-03 18:38:21 +0300

    updated output dir.

--- taxid_parse.py
+++ taxid_parse.py
@@ -33,28 +33,30 @@ def main():
     dframe = pd.read_csv(args.input, sep="\t")
 
     tempfile = args.input.split(".")[0].split("/")[-1]
-    dirname = args.input.split(tempfile)[0] + "outputs/"
+    # dirname = args.input.split(tempfile)[0] + "outputs/"
 
-    if not os.path.exists(dirname):
-        os.makedirs(dirname)
-    destname = dirname + tempfile + ".tsv"
+    if not os.path.exists("outputs/"):
+        os.makedirs("outputs/")
 
     if args.all_tied:
-        spu.parse_funcs(dframe, destname, spu.sort_tied, 1)
-        spu.make_krona(destname)
+        spu.parse_funcs(dframe, f"outputs/{tempfile}_all_tied.tsv",
+                        spu.sort_tied, 1)
+        spu.make_krona(f"outputs/{tempfile}_all_tied.tsv")
 
     if args.parse_conf is not None:
-        spu.parse_funcs(dframe, destname, spu.sort_conf, args.parse_conf)
-        spu.make_krona(destname)
+        spu.parse_funcs(dframe, f"outputs/{tempfile}_parse_conf.tsv",
+                        spu.sort_conf, args.parse_conf)
+        spu.make_krona(f"outputs/{tempfile}_parse_conf.tsv")
 
     if args.thresh_tied is not None:
-        spu.parse_funcs(dframe, destname, spu.sort_tied, args.thresh_tied)
-        spu.make_krona(destname)
+        spu.parse_funcs(dframe, f"outputs/{tempfile}_thresh_tied.tsv",
+                        spu.sort_tied, args.thresh_tied)
+        spu.make_krona(f"outputs/{tempfile}_thresh_tied.tsv")
 
     if args.assume_human:
-        spu.assume_human(dframe, destname)
+        spu.assume_human(dframe, f"outputs/{tempfile}_assume_human.tsv")
 
     if args.count_taxid:
-        spu.count_taxids(dframe, destname)
+        spu.count_taxids(dframe, f"outputs/{tempfile}_count_taxid.tsv")
 
 main()
